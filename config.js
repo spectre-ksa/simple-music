17791d05a4b1218c7228099de15059a8bd7f5a33
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2019-07-29 20:23:08 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    TOKEN: 'NTcxNzM1MjQyNTYyNDcwMDEy.XT8NQw.Vk6M15vuty1MqvPtr4l_Ovl53ug', 
-    YT_API_KEY: 'AIzaSyDFRGtM6WzFmqAXIgQcCB1_Q8L0NJj7a3M', 
-    prefix: 'R',
-    devs: ['434642311591493632']
-}
