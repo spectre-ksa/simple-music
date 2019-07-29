f44485db1460d02618f580dbb95d9cbe5263fdba
spectre-ksa <49376807+spectre-ksa@users.noreply.github.com>
2019-07-29 18:19:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'NTcxNzM1MjQyNTYyNDcwMDEy.XTnu9A.fWAbIkbtAt-REn8813paol5m4uo', 
+    TOKEN: 'NTcxNzM1MjQyNTYyNDcwMDEy.XT8NQw.Vk6M15vuty1MqvPtr4l_Ovl53ug', 
     YT_API_KEY: 'AIzaSyDFRGtM6WzFmqAXIgQcCB1_Q8L0NJj7a3M', 
     prefix: 'R',
     devs: ['434642311591493632']
